073b5c817c419f8511c3ac1861fdf20b51e19f8e
Francisco José Cruz Jiménez <Francisco Jos Cruz Jimnez>
2016-06-19 12:45:55 +0200

    Generando números aleatorios

--- js/functions.js
+++ js/functions.js
@@ -1,31 +1,40 @@
 console.log("Cargado JS");
 
 $(document).ready(function(){
-	$("#startGame").bind("click", function(){
-		cargarOperaciones();
-	});
+	$("#startGame").unbind().bind("click", function(){
+    window.location.href = "./game.html";
+    cargarOperaciones();
+  });
 });
 
-function testingAjax() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (xhttp.readyState == 4 && xhttp.status == 200) {
-      document.getElementById("ajaxTest").innerHTML = xhttp.responseText;
-    }
-  };
-  xhttp.open("GET", "./resources/ajax_info.txt", true);
-  xhttp.send();
-}
+// function testingAjax() {
+//   var xhttp = new XMLHttpRequest();
+//   xhttp.onreadystatechange = function() {
+//     if (xhttp.readyState == 4 && xhttp.status == 200) {
+//       document.getElementById("ajaxTest").innerHTML = xhttp.responseText;
+//     }
+//   };
+//   xhttp.open("GET", "./resources/ajax_info.txt", true);
+//   xhttp.send();
+// }
 
 function cargarOperaciones(){
-	console.log("Inicio Juego");
-	window.location.href = "./game.html";
+  console.log("Inicio Juego");
 	generarOperacion();
 }
 
 function generarOperacion(){
 	console.log("generarOperacion");
+  var num1 = getRandom(0,1000);
+  var num2 = getRandom(0,1000);
+  console.log(num1 +" " +num2);
 	$(document).ready(function(){
-		$("#num1").text("2");
+		$("#num1").html(num1);
+    $("#num2").html(num2);
 	});
+}
+
+function getRandom(min, max) {
+  var num =Math.random() * (max - min) + min;
+  return num.toFixed();
 }
